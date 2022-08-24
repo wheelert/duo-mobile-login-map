afe50dfdbf94242206ab264564741bb203412b7b
Thomas Wheeler <wheelert@wheelerwire.com>
2022-08-24 13:18:02 -0700

    Added time values and auth dates

--- MapGen.py
+++ MapGen.py
@@ -6,24 +6,24 @@ import sys
 import duo_client
 import json
 from six.moves import input
-import time 
+import time
+from datetime import datetime, date, timedelta
 import os
 import os.path
 
 from geopy.geocoders import Nominatim
+
 geolocator = Nominatim(user_agent="duo login app", timeout=None)
 
 # Configuration and information about objects to create.
 admin_api = duo_client.Admin(
-    ikey='CHANGEME',
-    skey='CHANGEME',
-    host='api-xxxxx.duosecurity.com',
+     key='CHANGEME',
+     skey='CHANGEME',
+     host='api-xxxxx.duosecurity.com',
 )
 
-
-
 _today = datetime.today()
-td = timedelta(30)
+td = timedelta(2)
 _date2 = _today - td
 
 _mintime = _date2.timestamp()
@@ -31,64 +31,70 @@ _mintime = _date2.timestamp()
 # Retrieve log info from API:
 logs = admin_api.get_authentication_log(mintime=_mintime)
 
-
 # Count authentications by country:
 counts = dict()
 sites = []
-
-
+log_count = len(logs)
+print(f"parsing {log_count} logs")
 for log in logs:
     city = log['location']['city']
     state = log['location']['state']
     country = log['location']['country']
-    
-    _timestamp = date.fromtimestamp( int(log['timestamp']) )
-    
-    if city != '':
-    	_loc = log['location']['city'] +' '+ log['location']['state'] + ',' + log['location']['country']
-    	if _loc not in sites:
-    		sites.append([_loc,log])
 
+    _timestamp = date.fromtimestamp(int(log['timestamp']))
+
+    if city != '':
+        _loc = log['location']['city'] + ' ' + log['location']['state'] + ',' + log['location']['country']
+        if _loc not in sites:
+            sites.append([_loc, log])
 
 _coords = []
 _string = ""
 
 _cnt = 1
 
-for _site in sites:
-	_data = geolocator.geocode(_site[0]).raw
-	_coords.append([_data['lon'],_data['lat']])
-	log = _site[1]
-	
-	if log['result'] == "SUCCESS":
-		_string += "var marker"+str(_cnt)+" = L.marker(["+ _data['lat'] +","+ _data['lon'] +"]).addTo(map)\n"
-	else:	
-		_string += "var marker"+str(_cnt)+" = L.marker(["+ _data['lat'] +","+ _data['lon'] +"],{color: 'red'}).addTo(map)\n"
-		
-	_datetime = datetime.fromtimestamp( int(log['timestamp']) )
-	_strtime = str(_datetime.month)+"/"+str(_datetime.day)+"/"+str(_datetime.year)+" "+str(_datetime.hour)+":"+str(_datetime.minute)+":"+str(_datetime.second)	
-		
-	_poptxt = " <b>"+str(log['email'])+"</b><br> <b>App:</b>"+str(log['integration'])+"<br> <b>IP:</b>"+str(log['ip'])+"<br /><b>Location:</b> "+log['location']['city']+","+log['location']['state']+"<br /><b>date:</b>"+_strtime
-		
-	_string += "marker"+str(_cnt)+".bindPopup('"+ _poptxt +"').openPopup();\n"
-	
-	time.sleep(2)
-	_cnt +=1
-	
-	
-print(_string)
+site_count = len(sites)
+site_time = (site_count * 2) / 60
+parse_now = datetime.now()
+parese_time = timedelta(minutes=site_time)
+parse_complete = parse_now + parese_time
 
+print(f"parsing {site_count} sites estimated time of completion {parse_complete}")
+
+for _site in sites:
+    _data = geolocator.geocode(_site[0]).raw
+    _coords.append([_data['lon'], _data['lat']])
+    log = _site[1]
+    log_time = datetime.fromtimestamp(log['timestamp'])
+
+    if log['result'] == "SUCCESS":
+        _string += "var marker" + str(_cnt) + " = L.marker([" + _data['lat'] + "," + _data['lon'] + "]).addTo(map)\n"
+    else:
+        _string += "var marker" + str(_cnt) + " = L.marker([" + _data['lat'] + "," + _data[
+            'lon'] + "],{color: 'red'}).addTo(map)\n"
+
+    # _datetime = datetime.fromtimestamp( int(log['timestamp']) )
+    # _strtime = str(_datetime.month)+"/"+str(_datetime.day)+"/"+str(_datetime.year)+" "+str(_datetime.hour)+":"+str(_datetime.minute)+":"+str(_datetime.second)
+
+    _poptxt = " <b>" + str(log['email']) + "</b><br> <b>App:</b>" + str(log['integration']) + "<br> <b>IP:</b>" + str(log['ip']) + "<br /><b>Location:</b> " + log['location']['city'] + "," + log['location']['state'] + "<br /><b>date:</b>" + str(log_time)
+
+    _string += "marker" + str(_cnt) + ".bindPopup('" + _poptxt + "').openPopup();\n"
+
+    time.sleep(2)
+    _cnt += 1
+
+print(_string)
 
 map_file = open("map_template.html", "r")
 html_data = map_file.read()
 map_file.close()
 
-map_data = html_data.replace('##MARKERS##',_string)
+map_data = html_data.replace('##MARKERS##', _string)
 
 if os.path.exists("map.html"):
-	os.remove("map.html")
+    os.remove("map.html")
 
-f = open("map.html","a")
+f = open("map.html", "a")
 f.write(map_data)
 f.close()
 
